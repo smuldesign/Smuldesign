d23ae27583c1ff41692639b6c999d8971bb45369
Rico Zethof <Ricozethof@gmail.com>
2019-07-31 11:43:43 +0200

    INIT

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+// Modules
+import { init as stars } from 'modules/stars'  ; // or './module'
+
+// Start the scripts
+document.addEventListener("DOMContentLoaded", function(){
+    // Handler when the DOM is fully loaded
+    stars();
+});
