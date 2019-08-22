3f67aac7f9d1bee6385a4fc40c92b3d80c0f2a2c
Rico Zethof <Ricozethof@gmail.com>
2019-08-22 20:50:27 +0200

    init

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php
+<!doctype html>
+<html lang="nl">
+<head>
+    <title>Smuldesign</title>
+    <?php include('static/parts/head.php')?>
+</head>
+<body class="homepage">
+<h1>yeet</h1>
+</body>
+</html>
